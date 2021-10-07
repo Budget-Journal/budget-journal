6ffe9ccf2639618222346b5fa79517a0df6888b5
Michael Werneke <mike.werneke@gmail.com>
2021-10-07 12:48:42 -0500

    Delete Card

--- src/components/CompletedGoal/GoalCardView.js
+++ src/components/CompletedGoal/GoalCardView.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Card } from '@mui/material';
+import { Card, CardContent, Grid, styled, Paper } from '@mui/material';
 import CompletedGoalsJournal from '../CompletedGoalsJournalPosts/CompletedGoalsJournalPosts';
 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function GoalCardView() {
 
@@ -17,17 +23,21 @@ function GoalCardView() {
         <div>
             {details.map(detail => (
                 <div>
-                    <Card>
-                        <p>{detail.name}</p>
-                        <p>{detail.price}</p>
-                        <p>{detail.reasons}</p> 
-                        <p>{detail.notes}</p>
-                    </Card>
+                    <Grid container>
+                        <Grid item xs={12} >
+                            <Item >
+                            Goal Name: {detail.name}
+                            <p>Item Expense: {detail.expense}</p>
+                            <p>Item Price: {detail.price}</p>
+                            <p>{detail.reasons}</p> 
+                            <p>Notes: {detail.notes}</p>
+                            <CompletedGoalsJournal journal={journal}/> 
+                            </Item>
+                        </Grid>
+                    </Grid>
                 </div>
             ))}
-            <div> 
-                <CompletedGoalsJournal journal={journal}/> 
-            </div>
+            
         </div>
     )
 }
