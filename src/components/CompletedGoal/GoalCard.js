5321629749fb2df9d3c78485d938bf70e062f777
Michael Werneke <mike.werneke@gmail.com>
2021-10-08 11:47:50 -0500

    Accomplished Goals and View Working with New Card Style

--- src/components/CompletedGoal/GoalCard.js
+++ src/components/CompletedGoal/GoalCard.js
@@ -1,36 +1,43 @@
 import React from "react";
-import { Grid, CardContent, Card, CardActions, Typography, Button } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 import { useSelector } from "react-redux";
 import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import { useHistory } from "react-router-dom";
+//Material UI Imports
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ShareIcon from '@mui/icons-material/Share';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    // card: {
-    //   padding: theme.spacing(2),
-    //   textAlign: 'center',
-    //   color: theme.palette.text.secondary,
-    // },
-  }));
+import VisibilityIcon from '@mui/icons-material/Visibility';
+//End Material UI Imports
+
+
+
+
 
 export default function GoalCard() {
   // Set hooks as variables
-   const classes = useStyles();
    const dispatch = useDispatch();
    const history = useHistory();
-
    const completedGoals= useSelector(store => store.completedGoal);
 
-  useEffect(() => {
+  useEffect((id) => {
     dispatch({
       type: 'FETCH_COMPLETED_GOALS'
           });
    }, []);
-  
+
    const handleView = (id) => {
     console.log('this goal id', id)
      dispatch({
@@ -45,8 +52,12 @@ export default function GoalCard() {
 
      history.push('/view')
    }
+    
+   
 
    const handleDelete =(id) => {
+     confirm('Are you sure?');
+
     dispatch({ 
      type: "DELETE_GOAL",
      payload: id,
@@ -54,32 +65,56 @@ export default function GoalCard() {
    };
   
     return (
-    <Grid item xs={6} sm={3}>
-      <Grid container>
-        {completedGoals.map((goal) => (
-          <Grid item xs>
-            {/* {" "} */}
-            &nbsp;
-            <Card key={goal.id}>
-              <CardContent className={classes.card}>
-                <Typography>Goal: {goal.name} </Typography>
+      <div>
+      {completedGoals.map(detail => (
+          <div>
+            <Grid container direction="column">
+              <Grid item xs={12}>
+               <Card  sx={{width: '100%'}}>
+                  <CardHeader
+                      avatar={
+                      <Avatar src="https://www.royalcaribbean.com/content/dam/royal/ports-and-destinations/destinations/alaska-cruise-tours/wonder-lake-denali-national-park-mountains-background.jpg"/>                                
+                      }
+                      action={
+                      <IconButton  aria-label="settings">
+                          <DeleteOutlineIcon onClick={handleDelete} />
+                      </IconButton>
+                      }
+                      title={detail.name}
+                      subheader="Completed Goal"
+                  />
+                  <CardMedia
+                      component="img"
+                      height="194"
+                      image="https://www.royalcaribbean.com/content/dam/royal/ports-and-destinations/destinations/alaska-cruise-tours/wonder-lake-denali-national-park-mountains-background.jpg"
+                      alt="Paella dish"
+                  />
+                  {/* <CardContent>
+                      <Typography variant="body2" color="text.secondary">
+                    
+                      </Typography>
+                  </CardContent> */}
+                  <CardActions disableSpacing>
 
-              </CardContent>
-              <CardActions>
-                {/* {" "} */}
-                &nbsp; &nbsp; &nbsp;
-                <Button onClick={() => handleView(goal.id)}>View</Button>
-                <Button onClick={() => handleDelete(goal.id)}
-                  
-                  color="secondary"
-                >
-                  Delete
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Grid>
+                      <IconButton aria-label="add to favorites">
+                      
+                      </IconButton> 
+
+                      <IconButton aria-label="share">
+                      <VisibilityIcon onClick={() => handleView(detail.id)}> </VisibilityIcon>
+                      
+                      </IconButton>
+ 
+                  </CardActions>
+                  </Card>
+                  </Grid>
+                  </Grid>
+          </div>
+      ))}
+
+      
+
+      
+  </div>
   );
 }
--- src/components/CompletedGoal/GoalCardView.js
+++ src/components/CompletedGoal/GoalCardView.js
@@ -17,6 +17,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 //End Material UI Imports
 
 
