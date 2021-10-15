4356307fb0b0f3b7675f8260e8cff002e2d56241
samson vang <vangsam2@gmail.com>
2021-10-15 16:27:15 -0500

    avatar

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -9,7 +9,7 @@ function LogOutButton(props) {
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       size="small"
-      variant="contained"
+      variant="text"
       color="secondary"
       className={props.className}
       onClick={() => dispatch({ type: 'LOGOUT' })}
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,7 +13,34 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CardMedia from '@mui/material/CardMedia';
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
+
+
 function Nav() {
+  //Handle menu from Avatar
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   //Grab from redux store
   const user = useSelector((store) => store.user);
   const activeGoals = useSelector(store => store.activeGoals);
@@ -45,32 +72,54 @@ function Nav() {
         <Toolbar>
           <img height="100px" width="auto" src={JournalLogo} alt="" />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          
+          **Render Balance / Budget**
           </Typography>
            {/* If no user is logged in, show these links */}
-
-        {user.id === null &&
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        }
+            {user.id === null &&
+              // If there's no user, show login/registration links
+              <Link className="navLink" to="/login">
+                Login / Register
+              </Link>
+            }
 
         {/* If a user is logged in, show these links */}
         {user.id && (
           <div className="user">
-            <h1 >
             {user ? <h6 className="userLog"> 
-            {user.username} 
+            <Stack direction="row" spacing={2}>
+                  <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                 <Avatar 
+                  alt={user.username}
+                  src="/static/images/avatar/1.jpg" />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => history.push('/createGoal')}>Profile</MenuItem>
+                <MenuItem onClick={() => dispatch({ type: 'LOGOUT' })}>Log Out</MenuItem>
+              </Menu> 
+            </Stack>
             <br/>
-            {/* ${user.total_budget}  */}
-            <br/>
-            Balance: {remainingBalance.toFixed(2)}
-            <br />
-            Goal Total: {totalGoalCostSum.toFixed(2)} 
-            <br/>
-            <LogOutButton className="navLink" />
-            </h6> : <h3>No user logged-in</h3>}</h1>
+            </h6> : <h3>No user logged-in</h3>}
           </div>
         )}
         </Toolbar>
